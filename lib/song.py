03037451ace58b89f199de17a8562e549203674e
jimmyjaxon <jimmyjaxon695@gmail.com>
2026-06-10 09:58:07 +0300

    Implement Song class with class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment total song count
+        Song.count += 1
+
+        # Track unique artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Track unique genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Track genre counts
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Track artist counts
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
